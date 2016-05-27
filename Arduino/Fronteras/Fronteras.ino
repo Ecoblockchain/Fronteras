850557d8323785bd2286a47869b09c934989f317
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-05-27 17:57:30 -0300

    tweaked delay and range

--- Arduino/Fronteras/Fronteras.ino
+++ Arduino/Fronteras/Fronteras.ino
@@ -3,7 +3,7 @@
 
 // Range [600,2200] ~180 deg ==> 9ms ~ 1 degree
 Servo mServo[2];
-const int SERVO_RANGE[2] = {40, 25}; // 90 ~10 deg
+const int SERVO_RANGE[2] = {50, 30}; // 90 ~10 deg
 
 // {BASE,LASER}
 const int SERVO_PIN[2] = {3, 5};
@@ -11,6 +11,7 @@ const int SERVO_CENTER[2] = {1460, 1530};
 const int LASER_PIN = 6;
 
 int mPosition = 0;
+const int mDELAY = 4000;
 
 void setup() {
   for (short i = 0; i < 2; i++) {
@@ -27,7 +28,7 @@ void loop() {
   for (short i = 0; i < 2; i++) {
     mServo[i].writeMicroseconds(SERVO_CENTER[i] + SERVO_RANGE[i] - mPoints[mPosition][i] * 2 * SERVO_RANGE[i]);
   }
-  delayMicroseconds(3333);
+  delayMicroseconds(mDELAY);
 
   if (mPosition == 0) {
     delay(500);
