89d490270cc60ded4cd9c37cf8999976b6c8cc88
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-05-28 00:46:58 -0300

    arduino: full-range and back-forth movement

--- Arduino/Fronteras/Fronteras.ino
+++ Arduino/Fronteras/Fronteras.ino
@@ -3,7 +3,7 @@
 
 // Range [600,2200] ~180 deg ==> 9ms ~ 1 degree
 Servo mServo[2];
-const int SERVO_RANGE[2] = {50, 30}; // 90 ~10 deg
+const int SERVO_RANGE[2] = {90, 40}; // 90 ~10 deg
 
 // {BASE,LASER}
 const int SERVO_PIN[2] = {3, 5};
@@ -11,7 +11,7 @@ const int SERVO_CENTER[2] = {1460, 1530};
 const int LASER_PIN = 6;
 
 int mPosition = 0;
-const int mDELAY = 4000;
+const int mDELAY = 3500; // [3333 - 5000]
 
 void setup() {
   for (short i = 0; i < 2; i++) {
@@ -23,6 +23,23 @@ void setup() {
 }
 
 void loop() {
+  back();
+}
+
+void back() {
+  analogWrite(LASER_PIN, 255);
+  for (short i = 0; i < 2; i++) {
+    mServo[i].writeMicroseconds(SERVO_CENTER[i] + SERVO_RANGE[i] - mPoints[abs(mPosition)][i] * 2 * SERVO_RANGE[i]);
+  }
+  delayMicroseconds(mDELAY);
+
+  mPosition = (mPosition + 1);
+  if (mPosition >= NUM_POINTS) {
+    mPosition = 1 - NUM_POINTS;
+  }
+}
+
+void forward() {
   analogWrite(LASER_PIN, (mPosition == 0) ? 0 : 255);
 
   for (short i = 0; i < 2; i++) {
@@ -31,7 +48,7 @@ void loop() {
   delayMicroseconds(mDELAY);
 
   if (mPosition == 0) {
-    delay(500);
+    delay(200);
   }
 
   mPosition = (mPosition + 1) % NUM_POINTS;
